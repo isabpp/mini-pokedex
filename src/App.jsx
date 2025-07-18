47a1ea64c8740e254b7ef894e915dbe682ed82b9
Isabella <114983093+pebsisab@users.noreply.github.com>
2025-07-18 17:49:14 +0300

    Locations page now loads the names of the locations

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Pokemon from './pages/Pokemon'
+import Locations from './pages/Locations'
 import './App.css'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <Pokemon />
+      <Locations />
     </>
   )
 }
--- src/pages/Locations.jsx
+++ src/pages/Locations.jsx
@@ -1,6 +1,38 @@
+import { fetchLocations } from "../api/pokemonApi";
+import React, {useState, useEffect} from 'react'; 
+
 function Locations() {
-    return (
-        <></>
+    const [location, setLocation] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const loadLocations = async () => {
+            try {
+                const data = await fetchLocations(20);
+                setLocation(data);
+                setLoading(false);
+            } catch (err) {
+                setError(err.message);
+                setLoading(false);
+            } 
+        };
+        loadLocations();
+    }, []);
+
+    if (loading) return <div>Loading the Locations...</div>;
+    if (error) return <div>Error: {error}</div>;
+
+    return(
+        <>
+            <div className='location-container'>
+                <div className='location-grid'>
+                    {location.map((loc) => (
+                        <p>{loc.name}</p>
+                    ))}
+                </div>
+            </div>
+        </>
     )
 }
 
