0c5701d6a6b72486495f055a06a3a603a7f5fd09
Isabella <114983093+pebsisab@users.noreply.github.com>
2025-08-05 00:49:45 +0300

    Implemented initial popup for pokemon cards

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,19 +1,26 @@
 import '../pokemon.css';
+import { useState } from 'react';
+import PokemonPopup from './PokemonPopup';
 
 function PokemonCard({pokemon}) {
+    const [popup, setPopup] = useState(false);
+
     return (
-        <div className="pokemon-card">
-            <img src={pokemon.sprites.front_default} alt={pokemon.name} className='pokemon-image'/>
-            <h3 className="pokemon-name"> {pokemon.name}</h3>
-            <div className="pokemon-types">
-                <span>type: </span>
-                {pokemon.types.map((typeInfo, index) => (
-                    <span key={index} className={`type-badge type-${typeInfo.type.name}`}>
-                        {typeInfo.type.name}
-                    </span>
-                ))}
+        <>
+            <div className="pokemon-card" onClick={() => setPopup(true)}>
+                <img src={pokemon.sprites.front_default} alt={pokemon.name} className='pokemon-image'/>
+                <h3 className="pokemon-name"> {pokemon.name}</h3>
+                <div className="pokemon-types">
+                    <span>type: </span>
+                    {pokemon.types.map((typeInfo, index) => (
+                        <span key={index} className={`type-badge type-${typeInfo.type.name}`}>
+                            {typeInfo.type.name}
+                        </span>
+                    ))}
+                </div>
             </div>
-        </div>
+            {popup && (<PokemonPopup pokemon={pokemon} onClose={() => setPopup(false)}/>)}
+        </>
     );
 }
 
--- src/components/PokemonPopup.jsx
+++ src/components/PokemonPopup.jsx
@@ -1,8 +1,16 @@
-function PokemonPopup() {
+function PokemonPopup({pokemon, onClose}) {
     return(
         <div className="popup-overlay">
             <div className="popup-content">
-                <button className="close-btn">close</button>
+                <button className="close-btn" onClick={onClose}>close</button>
+                <div className="abilities">
+                    <h3>Abilities</h3>
+                    <div className="ability-list">
+                        {pokemon.abilities.map((ability, index) => (
+                            <span key={index} className="ability-badge">{ability.ability.name}</span>
+                        ))}
+                    </div>
+                </div>
             </div>
         </div>
     );
