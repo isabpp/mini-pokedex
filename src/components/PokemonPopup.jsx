514e8f4f17fc910518ffefa81a2005a5c7d88d3f
Isabella <114983093+pebsisab@users.noreply.github.com>
2025-08-05 00:32:45 +0300

    Added pokemon popup component

---
+++ src/components/PokemonPopup.jsx
@@ -0,0 +1,11 @@
+function PokemonPopup() {
+    return(
+        <div className="popup-overlay">
+            <div className="popup-content">
+                <button className="close-btn">close</button>
+            </div>
+        </div>
+    );
+}
+
+export default PokemonPopup;
