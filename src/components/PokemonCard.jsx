f238a864eeb8d781194d29f4b412057f85610aac
Isabella <114983093+pebsisab@users.noreply.github.com>
2025-08-04 15:01:44 +0300

    Changed PokemonCard visuals

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -4,8 +4,9 @@ function PokemonCard({pokemon}) {
     return (
         <div className="pokemon-card">
             <img src={pokemon.sprites.front_default} alt={pokemon.name} className='pokemon-image'/>
-            <p>{pokemon.name}</p>
+            <h3 className="pokemon-name"> {pokemon.name}</h3>
             <div className="pokemon-types">
+                <span>type: </span>
                 {pokemon.types.map((typeInfo, index) => (
                     <span key={index} className={`type-badge type-${typeInfo.type.name}`}>
                         {typeInfo.type.name}
