07e5e10392ede9e7128470e599cd734de8b1f4be
Isabella <114983093+pebsisab@users.noreply.github.com>
2025-08-04 13:47:29 +0300

    Added types to PokemonCard

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -5,6 +5,13 @@ function PokemonCard({pokemon}) {
         <div className="pokemon-card">
             <img src={pokemon.sprites.front_default} alt={pokemon.name} className='pokemon-image'/>
             <p>{pokemon.name}</p>
+            <div className="pokemon-types">
+                {pokemon.types.map((typeInfo, index) => (
+                    <span key={index} className={`type-badge type-${typeInfo.type.name}`}>
+                        {typeInfo.type.name}
+                    </span>
+                ))}
+            </div>
         </div>
     );
 }
