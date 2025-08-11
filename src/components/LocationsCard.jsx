6cae6364d106f09071d1ed3f2d36de90ef0fda59
Isabella <114983093+pebsisab@users.noreply.github.com>
2025-08-11 18:52:13 +0300

    Added pokemon encounters to location cards

--- src/components/LocationsCard.jsx
+++ src/components/LocationsCard.jsx
@@ -5,6 +5,13 @@ function LocationsCard({location}) {
         <div className="bg-[#95f599] m-10 rounded-md p-3 min-w-3xs">
             <h3 className="text-xl font-medium uppercase">{location.name}</h3>
             <p>In city: {location.location.name}</p>
+            <div className="">
+                {location.pokemon_encounters.map((locInfo, index) => (
+                    <span key={index} className="">
+                        {locInfo.pokemon.name}
+                    </span>
+                ))}
+            </div>
         </div>
     )
 }
