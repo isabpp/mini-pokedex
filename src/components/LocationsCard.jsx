fcabf7db00db46e707b14c97ddbc412f4f310da9
Isabella <114983093+pebsisab@users.noreply.github.com>
2025-08-11 19:22:45 +0300

    Updated the style of location cards

--- src/components/LocationsCard.jsx
+++ src/components/LocationsCard.jsx
@@ -2,12 +2,13 @@ import '../locations.css';
 
 function LocationsCard({location}) {
     return (
-        <div className="bg-[#95f599] m-10 rounded-md p-3 min-w-3xs">
+        <div className="bg-[#95f599] m-10 rounded-md p-3 min-w-2xs max-w-80">
             <h3 className="text-xl font-medium uppercase">{location.name}</h3>
             <p>In city: {location.location.name}</p>
-            <div className="">
+            <p>Pokemons you can encounter: </p>
+            <div className="flex flex-wrap">
                 {location.pokemon_encounters.map((locInfo, index) => (
-                    <span key={index} className="">
+                    <span key={index} className="p-1 m-1 bg-[#bdffbf] rounded-sm capitalize">
                         {locInfo.pokemon.name}
                     </span>
                 ))}
