7aeafa0b07f5ea28ad650f5119a11b9ef7ecd3d8
Isabella <114983093+pebsisab@users.noreply.github.com>
2025-08-11 13:05:24 +0300

    Added an image and stats section to the popup

--- src/components/PokemonPopup.jsx
+++ src/components/PokemonPopup.jsx
@@ -4,13 +4,24 @@ function PokemonPopup({pokemon, onClose}) {
     return(
         <div className="popup-overlay">
             <div className="popup-content">
-                <button className="close-btn" onClick={onClose}>close</button>
-                <div className="abilities">
-                    <h3>Abilities</h3>
-                    <div className="ability-list">
-                        {pokemon.abilities.map((ability, index) => (
-                            <span key={index} className="ability-badge">{ability.ability.name}</span>
-                        ))}
+                <div className="title-bar">
+                    <h2>{pokemon.name}</h2>
+                    <button className="close-btn" onClick={onClose}>close</button>
+                </div>
+                <div className="pokemon-container">
+                    <img src={pokemon.sprites.front_default} alt={pokemon.name} className="pokemon-image"/>
+                    <div className="information-container">
+                        <div className="abilities">
+                            <h3>Abilities</h3>
+                            <div className="ability-list">
+                                {pokemon.abilities.map((ability, index) => (
+                                    <span key={index} className="ability-badge">{ability.ability.name}</span>
+                                ))}
+                            </div>
+                        </div>
+                        <div className="stats">
+                            <h3>Stats</h3>
+                        </div>
                     </div>
                 </div>
             </div>
