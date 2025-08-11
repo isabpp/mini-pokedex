cf0b2b1cbbb50d2c3857a15cd16581a636363bb4
Isabella <114983093+pebsisab@users.noreply.github.com>
2025-08-11 19:39:08 +0300

    Added the logo to the title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,11 @@ import { Link, NavLink } from 'react-router-dom';
 function NavBar() {
     return (
         <nav>
-            <h1 className='text-[50px]'>Mini Pokedex</h1>
+            <div className="flex flex-row items-center">
+                <h1 className='text-[50px]'>Mini Pokedex</h1>
+                <img src="https://www.pngarts.com/files/3/Pokeball-PNG-Transparent-Image.png" className="size-[50px] ml-3"></img>
+            </div>
+            
             <ul className="ul-container">
                 <li><NavLink to="/locations">Locations</NavLink></li>
                 <li><NavLink to="/">Pokemon</NavLink></li>
