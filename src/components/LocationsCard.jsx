ea54b7e0bf90581247dc2d62469e5fd504194e42
Isabella <114983093+pebsisab@users.noreply.github.com>
2025-08-13 17:49:29 +0300

    Implemented the use of the collapsible component

--- src/components/LocationsCard.jsx
+++ src/components/LocationsCard.jsx
@@ -3,12 +3,22 @@ import LocationCollapsible from './LocationCollapsible';
 import { useState } from 'react';
 
 function LocationsCard({location}) {
+    const [collapsible, setCollapsible] = useState(false);
+
+    const toggleCollapsible = () => {
+        setCollapsible(!collapsible);
+    };
+
     return (
-        <div className="bg-[#95f599] m-6 rounded-2xl p-4 pr-6 pl-6 w-2/3 flex flex-row justify-between border-4 border-green-800">
-            <h3 className="text-xl font-medium uppercase">{location.name}</h3>
-            <p>In city: {location.location.name}</p>
-            <button>more</button>
+        <div className="w-2/3"> 
+            <div className="bg-[#95f599] m-6 rounded-2xl p-4 pr-6 pl-6 flex flex-row justify-between border-4 border-green-800">
+                <h3 className="text-xl font-medium uppercase">{location.name}</h3>
+                <p>In city: {location.location.name}</p>
+                <button onClick={toggleCollapsible}>{collapsible ? 'Show less' : 'Show more'}</button>
+            </div>
+            {collapsible && <LocationCollapsible location={location}/>}
         </div>
+            
     )
 }
 
