198d218813aead3778b2f0c1d9673b8725576dee
Isabella <96348348+isabpp@users.noreply.github.com>
2025-08-16 00:09:06 +0300

    Updated popup font

--- src/components/PokemonPopup.jsx
+++ src/components/PokemonPopup.jsx
@@ -5,14 +5,14 @@ function PokemonPopup({pokemon, onClose}) {
         <div className="popup-overlay">
             <div className="popup-content">
                 <div className="title-bar">
-                    <h2 className="text-xl font-medium">{pokemon.name}</h2>
+                    <h2 className="text-3xl font-jersey font-medium">{pokemon.name}</h2>
                     <button className="close-btn" onClick={onClose}>close</button>
                 </div>
                 <div className="pokemon-container flex flex-row">
                     <img src={pokemon.sprites.front_default} alt={pokemon.name} className="w-2/5"/>
                     <div className="information-container flex flex-col w-3/5 m-2 bg-yellow-50 rounded-md">
                         <div className="abilities m-2">
-                            <h3 className="text-xl font-medium bg-gray-500/90 rounded-md p-1 text-white">Abilities</h3>
+                            <h3 className="text-3xl font-medium bg-gray-500/90 rounded-md p-1 text-white font-jersey">Abilities</h3>
                             <div className="ability-container mt-4">
                                 {pokemon.abilities.map((ability, index) => (
                                     <span key={index} className="ability-badge font-medium bg-gray-200/90">{ability.ability.name}</span>
@@ -20,7 +20,7 @@ function PokemonPopup({pokemon, onClose}) {
                             </div>
                         </div>
                         <div className="stats m-2">
-                            <h3 className="text-xl font-medium bg-gray-500/90 rounded-md p-1 text-white">Stats</h3>
+                            <h3 className="text-3xl font-medium bg-gray-500/90 rounded-md p-1 font-jersey text-white">Stats</h3>
                             <div className="stat-container flex flex-wrap mt-2">
                                 {pokemon.stats.map((statInfo, index) => (
                                     <span key={index} className={`stat-badge font-medium stat-${statInfo.stat.name}`}>
