f20aaa5195ac73bf9b85a34b24c943135bb7490a
Isabella <96348348+isabpp@users.noreply.github.com>
2025-08-16 00:01:57 +0300

    Updated navbar font

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function NavBar() {
     return (
         <nav className="bg-cyan-300">
             <div className="flex flex-row items-center">
-                <h1 className='text-[50px]'>Mini Pokedex</h1>
+                <h1 className='font-jersey text-[50px]'>Mini Pokedex</h1>
                 <img src="https://www.pngarts.com/files/3/Pokeball-PNG-Transparent-Image.png" className="size-[50px] ml-3"></img>
             </div>
             
