b953ce371dcc1ecbc50827246a28f9bf15ad422c
Isabella <96348348+isabpp@users.noreply.github.com>
2025-08-16 00:11:24 +0300

    Updated location card font

--- src/components/LocationsCard.jsx
+++ src/components/LocationsCard.jsx
@@ -12,7 +12,7 @@ function LocationsCard({location}) {
     return (
         <div className="w-2/3"> 
             <div className="bg-green-300 mx-6 mt-8 rounded-2xl p-4 pr-6 pl-6 flex flex-row justify-between border-4 border-green-700">
-                <h3 className="text-xl font-medium uppercase">{location.name}</h3>
+                <h3 className="text-2xl font-medium uppercase font-jersey">{location.name}</h3>
                 <p className="font-medium">In city: {location.location.name}</p>
                 <button onClick={toggleCollapsible} className="bg-green-100 rounded-sm px-2">{collapsible ? '▲' : '▼'}</button>
             </div>
