fe72908c038b653d9ea6b600d4bdcf4794d1e837
Isabella <96348348+isabpp@users.noreply.github.com>
2025-08-21 17:53:44 +0300

    Fixed location cards' element placement issue
    Switched to grid from flexbox so that the elements aren't shifted out of place across different rows.

--- src/components/LocationsCard.jsx
+++ src/components/LocationsCard.jsx
@@ -11,10 +11,10 @@ function LocationsCard({location}) {
 
     return (
         <div className="w-2/3"> 
-            <div className="bg-green-300 mx-6 mt-8 rounded-2xl p-4 pr-6 pl-6 flex flex-row justify-between border-4 border-green-700">
-                <h3 className="text-2xl font-medium uppercase font-jersey">{location.name}</h3>
-                <p className="font-medium">In city: {location.location.name}</p>
-                <button onClick={toggleCollapsible} className="bg-green-100 rounded-sm px-2">{collapsible ? '▲' : '▼'}</button>
+            <div className="bg-green-300 mx-6 mt-8 rounded-2xl p-4 grid grid-cols-3 gap-12 justify-center items-center border-4 border-green-700">
+                <h3 className="text-2xl font-medium uppercase font-jersey place-self-start">{location.name}</h3>
+                <p className="font-medium place-self-start">In city: {location.location.name}</p>
+                <button onClick={toggleCollapsible} className="bg-green-100 rounded-sm px-2 size-8 place-self-end">{collapsible ? '▲' : '▼'}</button>
             </div>
             {collapsible && <LocationCollapsible location={location}/>}
         </div>
