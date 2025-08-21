dec4ec70741af89928cada4ba729cde930ee81f5
Isabella <96348348+isabpp@users.noreply.github.com>
2025-08-21 16:52:58 +0300

    Made searchbar functional

--- src/pages/Pokemon.jsx
+++ src/pages/Pokemon.jsx
@@ -8,12 +8,15 @@ function Pokemon() {
     const [pokemon, setPokemon] = useState([]);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [searchInput, setSearchInput] = useState("");
+    const [filteredPokemon, setFilteredPokemon] = useState([]);
 
     useEffect(() => {
         const loadPokemon = async () => {
             try {
                 const data = await fetchPokemon(20);
                 setPokemon(data);
+                setFilteredPokemon(data);
                 setLoading(false);
             } catch (err) {
                 setError(err.message);
@@ -22,6 +25,13 @@ function Pokemon() {
         };
         loadPokemon();
     }, []);
+
+    useEffect(() => {
+        const filtered = pokemon.filter(p =>
+            p.name.toLowerCase().includes(searchInput.toLowerCase())
+        );
+        setFilteredPokemon(filtered);
+    }, [searchInput, pokemon]); //re runs when there has been a change in searchInput or pokemon
 
     if (loading) return <div className='text-white'>Loading the Pokemon...</div>;
     if (error) return <div className='text-white'>Error: {error}</div>;
@@ -32,10 +42,10 @@ function Pokemon() {
                 <div className='search-container bg-cyan-100 mt-3 p-2 flex flex-row rounded-lg items-center justify-center w-3/5'>
                     <p className='mx-3'>Searching for a specific pokémon?</p>
                     <FaSearch className='ml-3'/>
-                    <input type='text' className='bg-white mx-3 rounded-md'></input>
+                    <input type='text' value={searchInput} onChange={(e) => setSearchInput(e.target.value)} className='bg-white mx-3 rounded-md'></input>
                 </div>
                 <div className='pokemon-grid flex flex-wrap flex-row items-center justify-center p-2 w-2/3'>
-                    {pokemon.map((p) => (
+                    {filteredPokemon.map((p) => (
                         <PokemonCard pokemon={p} key={p.id} />
                     ))}
                 </div>
